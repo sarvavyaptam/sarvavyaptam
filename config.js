6e61c3965cd6a1dde1a55a77123dd0a0eeb4eacb
Deeptam Research Foundation <deeptamresearchfoundationl@gmail.com>
2022-02-26 03:39:12 +0530

    hi;

--- config.js
+++ config.js
@@ -1,4 +1,6 @@
 const config = {
+    // mypass = AAYUSH@2021@AAYUSH
+    // "db_key" : "mongodb+srv://deeptamresearchfoundation:bZTAyJ5rOnAgOXJs@space.w2arw.mongodb.net/sarvavyaptam?retryWrites=true&w=majority"
     "db_key": "mongodb://localhost:27017/sarvavyaptam"
 }
 
